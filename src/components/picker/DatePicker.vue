97810df2f6a8f0f093ffeea329ab5735a826325a
zhoujie <zhoujie@antiy.cn>
2017-10-10 18:37:40 +0800

    add yyyy and mm

--- src/components/picker/DatePicker.vue
+++ src/components/picker/DatePicker.vue
@@ -2,13 +2,16 @@
     <div class="datepicker-layout">
         <input type="text" v-model="yyyy">
         <input type="text" v-model="mm">
-        <button @click="getDayObjectsList()">set mm</button>
+        <button @click="daysList()">set mm</button>
         
+        <div class="date-picker-month-select">
+            {{display_yyyy}} - {{display_mm}}
+        </div>
         <div class="datepicker-days-grid">
             <template v-for="(week_days,w) in days_obj_list">
                 <div class="datepicker-week-row" :key="w">
                     <template v-for="(day_obj,d) in week_days">
-                        <div class="datepicker-day-coll" :class="{'datepicker-curr_mm-day':day_obj.mm==getMonth(curr_mm)}" :key="d">
+                        <div class="datepicker-day-coll" :class="{'datepicker-curr__mm-day':day_obj.mm==display_mm,'datepicker-curr__dd':isCurrDay(day_obj)}" :key="d">
                             {{day_obj.dd}}
                         </div>
                     </template>
@@ -31,7 +34,9 @@ export default {
             dd:0,
             ww:0,
 
-            curr_mm:0,
+            display_yyyy:0,
+            display_mm:0,
+            display_dd:0,
 
             days_obj_list:[],
 
@@ -58,7 +63,10 @@ export default {
         }
     },
     methods:{
-        getDayObjectsList(){
+        isCurrDay(date_obj){
+            return date_obj.yyyy==this.display_yyyy&&date_obj.mm==this.display_mm&&date_obj.dd==this.display_dd;
+        },
+        daysList(){
             var self = this;
             var year = parseInt(self.yyyy);
             var month = parseInt(self.mm);
@@ -69,48 +77,45 @@ export default {
             d.setDate(1);
 
             var prev_mm = month-1;
-            var curr_mm = self.curr_mm = month;
+            var curr_mm = month;
             var next_mm = month+1;
 
             var prev_date = self.getLastDayDate(year,prev_mm);
             var curr_date = self.getLastDayDate(year,curr_mm);
             var next_date = self.getLastDayDate(year,next_mm);
 
+            self.display_yyyy = curr_date.getFullYear();
+            self.display_mm = self.getMonth(curr_date.getMonth());
+            self.display_dd = self.dd;
+
             var prev_days = prev_date.getDate();
             var curr_days = curr_date.getDate();
             var next_days = next_date.getDate();
 
             var curr_week_start = d.getDay();
-            var prev_days_list = [];
-            for(let i=(prev_days-curr_week_start+1);i<=prev_days;i++){
-                var yyyy = prev_date.getFullYear();
-                var mm = self.getMonth(prev_date.getMonth());
-                prev_days_list.push({yyyy,mm,dd:i});
-            }
-
-            var curr_days_list = [];
-            for(let i=1;i<=curr_days;i++){
-                var yyyy = curr_date.getFullYear();
-                var mm = self.getMonth(curr_date.getMonth());
-                curr_days_list.push({yyyy,mm,dd:i});
-            }
-
             d.setDate(curr_days);
             var curr_week_end = d.getDay();
-            var next_days_list = [];
-            for(let i=1;i<=(6-curr_week_end);i++){
-                var yyyy = next_date.getFullYear();
-                var mm = self.getMonth(next_date.getMonth());
-                next_days_list.push({yyyy,mm,dd:i});
-            }
+
+            var prev_days_list = self.getDaysList(prev_date,(prev_days-curr_week_start+1),prev_days);
+            var curr_days_list = self.getDaysList(curr_date,1,curr_days);
+            var next_days_list = self.getDaysList(next_date,1,(6-curr_week_end));
             
             var days_obj_list = prev_days_list.concat(curr_days_list.concat(next_days_list));
-            var temp_arr = [];
-            for(let i=0;i<days_obj_list.length/7;i++){
-                temp_arr.push(days_obj_list.slice(i*7,((i+1)*7)));
-            }
 
-            self.days_obj_list = temp_arr;
+            self.days_obj_list = [];
+            for(let i=0;i<days_obj_list.length/7;i++){
+                self.days_obj_list.push(days_obj_list.slice(i*7,((i+1)*7)));
+            }
+        },
+        getDaysList(date_obj,start,end){
+            var self = this;
+            var days_list = [];
+            for(let i=start;i<=end;i++){
+                var yyyy = date_obj.getFullYear();
+                var mm = self.getMonth(date_obj.getMonth());
+                days_list.push({yyyy,mm,dd:i});
+            }
+            return days_list;
         },
         getLastDayDate(year,month){
             var self = this;
@@ -142,13 +147,14 @@ export default {
         self.dd = self.init_date.getDate();
         self.ww = self.init_date.getDay();
         
-        self.getDayObjectsList();
+        self.daysList();
     }
 }
 </script>
 <style scoped>
 .datepicker-layout{
     display: inline-block;
+    font-size: 0.75em;
 }
 
 .datepicker-week-row{
@@ -163,9 +169,14 @@ export default {
     flex: 1;
     margin: 1px;
     background-color: #eee;
+    border-radius: 4px;
 }
 
-.datepicker-day-coll.datepicker-curr_mm-day{
+.datepicker-day-coll.datepicker-curr__mm-day{
     background-color: #fff;
+}
+.datepicker-day-coll.datepicker-curr__mm-day.datepicker-curr__dd{
+    background-color: #aaa;
+    color: #fff;
 }
 </style>
